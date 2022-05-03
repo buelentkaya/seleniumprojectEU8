6f672affe5437bea0b2a08e033410193ac2a3028
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-05-03 18:33:26 +0200

    FindElement method and 8 locators

---
+++ src/test/java/com/cydeo/tests/day2_getText_getAttribute/T1_CydeoVerification.java
@@ -0,0 +1,49 @@
+package com.cydeo.tests.day2_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_CydeoVerification {
+    public static void main(String[] args) {
+
+//TC #1: Cydeo practice tool verifications
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to https://practice.cydeo.com
+        driver.navigate().to("https://practice.cydeo.com");
+
+        //3. Verify URL contains
+        //Expected: cydeo
+        String expectedURL = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+
+        System.out.println(actualURL);
+        if(actualURL.contains(expectedURL)){
+            System.out.println("URL verification PASSED! ");
+        }else{
+            System.out.println("URL verification FAILED! ");
+        }
+
+
+        // 4. Verify title:
+        //Expected: Practice
+        String expectedTitle="Practice";
+        String actualTitle=driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title Verification Passed!");
+        }else{
+            System.out.println("Title Verification failed!");
+        }
+
+
+
+
+
+driver.close();
+    }
+}
---
+++ src/test/java/com/cydeo/tests/day2_getText_getAttribute/T3_GoogleSearch.java
@@ -0,0 +1,14 @@
+package com.cydeo.tests.day2_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T3_GoogleSearch {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+}
