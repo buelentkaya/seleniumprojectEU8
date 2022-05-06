3f645dcf6568e084fcfdb87dfeac8744c7861e85
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-05-06 19:39:57 +0200

    cssSelector

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T3_getAttribute_cssSelector {
     public static void main(String[] args) {
@@ -15,15 +17,28 @@ public class T3_getAttribute_cssSelector {
 
         //3- Verify “Log in” button text is as expected:
         //Expected: Log In
-        //WebElement signInButton = driver.findElement(By.className("login-btn"));
+       // WebElement signInButton = driver.findElement(By.className("login-btn"));
 
         //LOCATING THE SAME WEB ELEMENT USING DIFFERENT ATTRIBUTE VALUES.
         //	                   tagName[attribute='value']
         //	                   input[class='login-btn']
+
         // LOCATED USING CLASS ATTRIBUTE
         //WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
+
+
         // LOCATED USING TYPE ATTRIBUTE
         //WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
+      //  String logIn= signInButton.getAttribute("type");// to get what it is written in "type" attribute
+       // System.out.println("logIn = " + submit;
         // LOCATED USING VALUE ATTRIBUTE
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+        String logIn=signInButton.getAttribute("value");
+        if(logIn.equals("Log In")){
+            System.out.println("Log In button text verification passed!");
+        }else{
+            System.out.println("Log In button text verification failed!");
+        }
+        driver.close();
     }
 }
