8ba47c4bdc79e464e0c9751b0e8398d5b3c74f5c
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-05-06 15:55:40 +0200

    webdriverfactary utility

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+public class T2_getText_getAttribute {
+    public static void main(String[] args) {
+
+        //TC #2: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a Chrome browser
+
+
+
+    }
+}
