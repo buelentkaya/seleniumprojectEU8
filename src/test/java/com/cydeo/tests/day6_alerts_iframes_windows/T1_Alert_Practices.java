d54feea60d01fb4b89c804dafe482e1c189222f6
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-05-18 22:27:47 +0200

    information confirmation and prompt alert task done

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -2,10 +2,7 @@ package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.HandleWait;
 import com.cydeo.utilities.WebDriverFactory;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -27,13 +24,14 @@ public class T1_Alert_Practices {
         driver.get("https://practice.cydeo.com/javascript_alerts");
 
     }
-@AfterMethod
-    public void tearsDownMethod(){
-        //driver.close();
-}
+
+    @AfterMethod
+    public void tearsDownMethod() {
+        driver.quit();
+    }
 
     @Test
-    public void alert_Test1() {
+    public void Information_alert() {
 
         //3. Click to “Click for JS Alert” button
         driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
@@ -48,33 +46,57 @@ public class T1_Alert_Practices {
         //5. Verify “You successfully clicked an alert” text is displayed.
         String expectedText = "You successfully clicked an alert";
         WebElement resultTest = driver.findElement(By.id("result"));
-       // WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+        // WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
 
-        Assert.assertTrue(resultTest.isDisplayed(),"Result text is NOT displayed.");
+        Assert.assertTrue(resultTest.isDisplayed(), "Result text is NOT displayed.");
 
         String actualText = resultTest.getText();
         System.out.println("actualText = " + actualText);
 
-        Assert.assertEquals(actualText,expectedText,"Text Verification failed");
-
-
+        Assert.assertEquals(actualText, expectedText, "Text Verification failed");
 
 
     }
 
 
-    @Test (priority = 2)
-    public void alert_Test2() {
-        //3. Click to “Click for JS Alert” button
+    @Test(priority = 2)
+    public void Confirmation_alert() {
+        //3. Click to “Click for JS Confirm” button
         driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();// orada yazan yaziyi direk al
         HandleWait.staticWait(1);
 
         Alert alert = driver.switchTo().alert();
-        //4. Click to OK button from the alert
+        // 4. Click to OK button from the alert
+        alert.accept();
+        //   5. Verify “You clicked: Ok” text is displayed.
+        String expectedText = "You clicked: Ok";
+        String actualText = driver.findElement(By.id("result")).getText();
+        System.out.println("actualText = " + actualText);
+        Assert.assertEquals(expectedText, actualText, " Verificaion Confirmation Alert Text failed!");
+
+
+    }
+
+    @Test(priority = 3)
+    public void prompt_alert() {
+
+        //Click to “Click for JS Prompt” button
+        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+
+
+        //4. Send “hello” text to alert
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("hello");
+
+        //5. Click to OK button from the alert
         alert.accept();
 
-        String expectedText="You clicked: Ok";
+        //6. Verify “You entered: hello” text is displayed.
+        String expectedText = "You entered: hello";
+        String actualText = driver.findElement(By.xpath("//p[@id='result']")).getText();
+        System.out.println("actualText = " + actualText);
 
+        Assert.assertEquals(expectedText, actualText, " Verificaion Prompt Alert Text failed!");
 
 
     }
