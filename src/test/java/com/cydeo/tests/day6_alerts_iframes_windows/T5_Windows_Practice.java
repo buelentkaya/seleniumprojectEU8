1ed84c2aaa0de2b421aa01a9fce151d8a21683b7
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-05-18 18:37:21 +0200

    window task

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -1,4 +1,53 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class T5_Windows_Practice {
+    WebDriver driver;
+
+    //@BeforeMethod
+    @BeforeClass
+    public void setupMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to website: https://practice.cydeo.com/iframe
+        driver.get("https://practice.cydeo.com/iframe");
+    }
+
+    @Test
+    public void multipleWindow_test(){
+
+        //Storing the main page's window handle as string is
+        // good practice for future re-usable purposes
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
