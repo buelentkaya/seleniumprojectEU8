a23f4bf1a6d7ee22910138ad74ed2084dfa24f23
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-05-30 23:49:16 +0200

    LearningProperties

---
+++ src/test/java/com/cydeo/tests/DAY8_WebTables_Properties/LearningProperties.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.DAY8_WebTables_Properties;
+
+import org.testng.annotations.Test;
+
+public class LearningProperties {
+
+    @Test
+    public void javaPropertiesReadingTest(){
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
